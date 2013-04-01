b24b0815315b0713dbe29c1eb8838da4863e21b1
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2013-04-01 18:39:05 +0400

    some xcode files

---
+++ geotracker/Classes/Protocols/STGTSessionManagement.h
@@ -0,0 +1,29 @@
+//
+//  STGTSessionManagement.h
+//  geotracker
+//
+//  Created by Maxim Grigoriev on 3/24/13.
+//  Copyright (c) 2013 Maxim Grigoriev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol STGTSessionManager <NSObject>
+
+- (void)startSessionForUID:(NSString *)uid authDelegate:(id)authDelegate settings:(NSDictionary *)settings;
+- (void)stopSessionForUID:(NSString *)uid;
+- (void)sessionCompletionFinished:(id)session;
+- (void)cleanCompleteSessions;
+
+@end
+
+
+@protocol STGTSession <NSObject>
+
++ (id <STGTSession>)initWithUID:(NSString *)uid authDelegate:(id)authDelegate settings:(NSDictionary *)settings;
+- (void)completeSession;
+
+@property (strong, nonatomic) UIManagedDocument *document;
+
+
+@end
